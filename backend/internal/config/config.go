d51a9ecd3c0268cbfc0e65e57d54a6f8dc81a2ea
sachin <sachingunawardhana04@gmail.com>
2026-09-13 13:03:06 +0530

    add database configuration

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -3,14 +3,24 @@ package config
 import "os"
 
 type Config struct {
-	AppPort string
-	AppEnv  string
+	AppPort    string
+	AppEnv     string
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
 }
 
 func Load() Config {
 	return Config{
-		AppPort: getEnv("APP_PORT", "8081"),
-		AppEnv:  getEnv("APP_ENV", "development"),
+		AppPort:    getEnv("APP_PORT", "8081"),
+		AppEnv:     getEnv("APP_ENV", "development"),
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "armalora"),
+		DBPassword: getEnv("DB_PASSWORD", ""),
+		DBName:     getEnv("DB_NAME", "armalora_db"),
 	}
 }
 
